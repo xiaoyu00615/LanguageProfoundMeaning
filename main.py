c0dcc028fd34453b29fa5f2e278170ba98b37296
xiaoyu00615 <daizy2005@qq.com>
2025-09-13 18:25:07 +0800

    change message

--- main.py
+++ main.py
@@ -1,48 +1,12 @@
 # 这是一个示例 Python 脚本。
 import file_window.window as win
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
 
 def main_program():
-    print('主程序')
-    print_hi("你好")
-    win.create_window()
-    shen_fn()
-
-
-def shen_fn():
-    print("2025.9.13.")
-    print("感谢代子岩助力，成功验证SSH！OvO")
     win.create_window()
 
-    print("新代码")
-    print('新新新！')
 
 
-    print("新分支!!!!!!!!!!!!!!")
-    print('新代码!!!!!!!!!!!!!!')
-    print('新代码!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
-    print("本行代码于2025.9.13 17：28分书写")
-    print("本行代码于2025.9.13 17：29分书写")
-    print("本行代码于2025.9.13 17：30分书写")
-
-    print(1237982748932)
-    print("cnsdngfvjdksgvdng")
-
-    print("ajsdklfasjkljkl!!!!!!!!!!!!!!!!!!!!!!!")
-
-    print(8797834534546565)
-    print("jsakfhdsjkfhnkdjsnfnmsdlkfnljk")
-
-    print("本行代码于2025.9.13 17：33分书写")
-
-
-# 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     main_program()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
